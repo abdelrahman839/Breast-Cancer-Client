368516ff47f92e08c071f91bada0878d64984ef6
abdelrahman839 <abdelrahmanabdallah9800@gmail.com>
2022-03-11 03:18:29 +0200

    update design

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// // toggle nav active class
-// let navItem = document.querySelectorAll('.nav-item');
-// navItem.forEach((element, index) => {
-//     element.addEventListener('click', function () {
-//         element.classList.add('active');
-//         for (let i = 0; i < navItem.length; i++) {
-//             if (i != index) {
-//                 navItem[i].classList.remove('active')
-//             }
-//         }
+const checkCancer = () => {
+    const selected = $("input[type='radio']:checked");
+    if (selected.length != 6) {
+        document.querySelector('.error-lable').style.display = "block";
+    } else {
+        let check = 0;
+        for (let i = 0; i < selected.length; i++) {
+            console.log(selected[i].value)
+            if (selected[i].value == "Yes") {
+                check += 1;
+            }
+        }
 
+        if (check >= 3) {
+            window.location.href = "/public/hospitals.html";
+        } else {
+            window.location.href = "/public/blog.html";
+        }
+    }
 
-//     })
-// });
+}
