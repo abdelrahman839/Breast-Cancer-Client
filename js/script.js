9a93395c130756a3c3185c2b891d29dcad2bef70
abdelrahman839 <abdelrahmanabdallah9800@gmail.com>
2022-03-28 01:42:17 +0200

    check token

--- js/script.js
+++ js/script.js
@@ -25,17 +25,22 @@ const checkCancer = () => {
 
 async function onSignIn(googleUser) {
     var profile = await googleUser.getBasicProfile();
-    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    // console.log('Full Name: ' + profile.getName());
-    // console.log('Given Name: ' + profile.getGivenName());
-    // console.log('Family Name: ' + profile.getFamilyName());
-    // console.log("Image URL: " + profile.getImageUrl());
-    // console.log("Email: " + profile.getEmail());
-
     // // The ID token you need to pass to your backend:
     var id_token = await googleUser.getAuthResponse().id_token;
     $.post(`http://localhost:5000/user/sign-in?id_token=${id_token}`);
-
+    localStorage.setItem('Breast-Cancer-Token', id_token);
 
 }
+
+const checkToken = () => {
+    if (localStorage.getItem('Breast-Cancer-Token') == null) {
+        document.querySelector('.required-layer').style.display = 'flex';
+    }
+    else {
+        document.querySelector('.required-layer').style.display = 'none';
+
+    }
+}
+checkToken();
+
 
