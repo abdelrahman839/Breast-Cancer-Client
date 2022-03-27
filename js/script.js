f3f34a330f3bd8b1f0d57cae51b5ceb5a9baeb51
abdelrahman839 <abdelrahmanabdallah9800@gmail.com>
2022-03-27 20:55:49 +0200

    yy

--- js/script.js
+++ js/script.js
@@ -23,36 +23,36 @@ const checkCancer = () => {
 
 
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+async function onSignIn(googleUser) {
+    var profile = await googleUser.getBasicProfile();
+    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    // console.log('Full Name: ' + profile.getName());
+    // console.log('Given Name: ' + profile.getGivenName());
+    // console.log('Family Name: ' + profile.getFamilyName());
+    // console.log("Image URL: " + profile.getImageUrl());
+    // console.log("Email: " + profile.getEmail());
 
     // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
+    var id_token = await googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
 
 
 
     const url = 'http://localhost:5000/user/sign-in';
 
-    fetch(url, {
+    await fetch(url, {
         method: 'POST',
         headers: {
-          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
         },
         credentials: 'include',
         body: id_token,
-      })
-     
+    })
+
 
 
     // const http = new XMLHttpRequest();
-   
+
     // http.open('POST', url, true);
 
     // //Send the proper header information along with the request
