a650acbf842f7e7866b573858f1b025af48ec716
abdelrahman839 <abdelrahmanabdallah9800@gmail.com>
2022-03-28 02:04:49 +0200

    lol

--- js/script.js
+++ js/script.js
@@ -23,24 +23,15 @@ const checkCancer = () => {
 
 
 
-
-   
-    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    // console.log('Full Name: ' + profile.getName());
-    // console.log('Given Name: ' + profile.getGivenName());
-    // console.log('Family Name: ' + profile.getFamilyName());
-    // console.log("Image URL: " + profile.getImageUrl());
-    // console.log("Email: " + profile.getEmail());
-
-
-
 async function onSignIn(googleUser) {
     var profile = await googleUser.getBasicProfile();
-    console.log(profile)
     // // The ID token you need to pass to your backend:
     var id_token = await googleUser.getAuthResponse().id_token;
     $.post(`http://localhost:5000/user/sign-in?id_token=${id_token}`);
     localStorage.setItem('Breast-Cancer-Token', id_token);
+    localStorage.setItem('First-Name', profile.VX);
+    localStorage.setItem('Last-Name', profile.iW);
+    localStorage.setItem('Email', profile.tv);
     checkToken();
 }
 
